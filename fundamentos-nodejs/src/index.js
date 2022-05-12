4e9c342c3d886da9526af0e78651dd17a6187ce2
André Quintino <pandrequintino@gmail.com>
2022-05-12 23:53:27 +0100

    Chapter I: M02: 02 Adicionando o Nodemon na aplicação

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+// localhost:3333
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World Ingite!' });
 });
