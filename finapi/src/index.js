6463c41481f9051dabcec8f8a92fc279bf51ba03
André Quintino <pandrequintino@gmail.com>
2022-05-13 19:48:50 +0100

    Chapter I: M02: 02 Cadastro de conta

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * nif: string
+ * name: string
+ * id: uuid
+ * statement: []
+ */
+app.post('/accounts', (request, response) => {
+  const {
+    body: { nif, name },
+  } = request;
+
+  const id = uuidv4();
+
+  customers.push({ nif, name, id, statement: [] });
+
+  return response.sendStatus(201); //* Created
+});
 
 app.listen(3333);
