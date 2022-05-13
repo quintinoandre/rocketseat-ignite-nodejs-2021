cb3fce322597219eeb5ca2be4b50d4e4c55108b2
André Quintino <pandrequintino@gmail.com>
2022-05-13 01:49:07 +0100

    Chapter I: M02: 05 Tipos de parâmetros

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * GET - Procurar uma informação dentro do servidor
  * POST - Inserir uma informação no servidor
@@ -10,17 +12,39 @@ const app = express();
  * DELETE - Remover uma informação no servidor
  */
 
+/**
+ * Tipos de parâmetros:
+ *
+ * Route Params => Parâmetros que vem na própria rota e que identificam um recurso (Editar/Remover/Procurar)
+ * Query Params => Parâmetros que vem na própria rota e que são utilizados para filtrar os resultados (Paginação, Filtro)
+ * Body Params => Parâmetros/objetos (JSON) que vem no corpo da requisição (Criação/Edição)
+ */
+
 // localhost:3333
 
 app.get('/courses', (request, response) => {
+  const { query } = request;
+
+  console.log(query);
+
   return response.json(['Course 1', 'Course 2', 'Course 3']);
 });
 
 app.post('/courses', (request, response) => {
+  const { body } = request;
+
+  console.log(body);
+
   return response.json(['Course 1', 'Course 2', 'Course 3', 'Course 4']);
 });
 
 app.put('/courses/:id', (request, response) => {
+  const {
+    params: { id },
+  } = request;
+
+  console.log(id);
+
   return response.json(['Course 6', 'Course 2', 'Course 3', 'Course 4']);
 });
 
