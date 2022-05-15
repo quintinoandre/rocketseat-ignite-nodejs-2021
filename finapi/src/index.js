0163e454bcaf0596e6f05bf6d9f43254bbf53faf
André Quintino <pandrequintino@gmail.com>
2022-05-15 17:55:24 +0100

    Chapter I: M02: 07 Criando depósito na conta

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -7,6 +7,11 @@ app.use(express.json());
 
 const customers = [];
 
+const STATEMENT_TYPES = Object.freeze({
+  credit: 'credit',
+  debit: 'debit',
+});
+
 // Middleware
 function verifyIfExistsAccountNIF(request, response, next) {
   const {
@@ -46,6 +51,24 @@ app.get('/statement', verifyIfExistsAccountNIF, (request, response) => {
   const { customer } = request;
 
   return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountNIF, (request, response) => {
+  const {
+    body: { description, amount },
+    customer,
+  } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: STATEMENT_TYPES.credit,
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.sendStatus(201); //* Created
 });
 
 const PORT = 3333;
