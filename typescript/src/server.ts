cbc43364e9fb343a7c400974f5cd892953c554af
André Quintino <pandrequintino@gmail.com>
2022-05-18 22:23:45 +0100

    Chapter II: M01.1: 03 Criando projeto com typescript

---
+++ typescript/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
