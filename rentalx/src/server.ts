51f6bb39e01cf37a402d20f671af2135f140e63b
André Quintino <pandrequintino@gmail.com>
2022-05-19 23:05:02 +0100

    Chapter II: M01.2: 02 Debugando a aplicação

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -2,8 +2,18 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
 	return response.json({ message: 'Hello World!' });
+});
+
+app.post('/courses', (request, response) => {
+	const {
+		body: { name },
+	} = request;
+
+	return response.json({ name });
 });
 
 const PORT = 3333;
