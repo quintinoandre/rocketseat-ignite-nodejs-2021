7628d150550970c491e712cfa76d46f7f0e95a84
André Quintino <pandrequintino@gmail.com>
2022-05-20 18:34:17 +0100

    Chapter II: M01.5: 02 Criando upload de arquivos

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import {
 	createCategoryController,
@@ -7,12 +8,22 @@ import {
 
 const categoriesRoutes = Router();
 
+const upload = multer({ dest: './tmp' });
+
 categoriesRoutes.post('/', (request, response) => {
 	return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
 	return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+	const { file } = request;
+
+	console.log(file);
+
+	return response.send();
 });
 
 export { categoriesRoutes };
