a412049c2aef6ff8991715aff0fb281c92f87676
André Quintino <pandrequintino@gmail.com>
2022-05-23 20:14:55 +0100

    Chapter II: M01.6: 05 Removendo os arquivos de upload

--- rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -27,7 +27,11 @@ class ImportCategoryUseCase {
 
 					categories.push({ name, description });
 				})
-				.on('end', () => resolve(categories))
+				.on('end', () => {
+					fs.promises.unlink(file.path);
+
+					resolve(categories);
+				})
 				.on('error', (error) => reject(error));
 		});
 	}
