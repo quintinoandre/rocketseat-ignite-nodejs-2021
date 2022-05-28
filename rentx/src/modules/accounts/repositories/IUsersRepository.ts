a2bebc6d5c35a1908c58bc4d367298a52ab6065a
André Quintino <pandrequintino@gmail.com>
2022-05-28 02:55:50 +0100

    Chapter III: M01.4: 05 Criptografrar senha

--- rentx/src/modules/accounts/repositories/IUsersRepository.ts
+++ rentx/src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from '../dtos';
+import { User } from '../entities';
 
 interface IUsersRepository {
 	create(data: ICreateUserDTO): Promise<void>;
+	findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
--- rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos';
@@ -11,7 +12,15 @@ class CreateUserUseCase {
 	) {}
 
 	async execute(data: ICreateUserDTO): Promise<void> {
-		await this.usersRepository.create({ ...data });
+		const userAlreadyExists = await this.usersRepository.findByEmail(
+			data.email
+		);
+
+		if (userAlreadyExists) throw new Error('User already exists');
+
+		const passwordHash = await hash(data.password, 8);
+
+		await this.usersRepository.create({ ...data, password: passwordHash });
 	}
 }
 
