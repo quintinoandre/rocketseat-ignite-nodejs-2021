b9ecd2b552c1a6faf5f61619b093edbcf3e0712c
André Quintino <pandrequintino@gmail.com>
2022-06-27 23:54:00 +0100

    Módulo Prisma IO: 05.1 Criar tabela com relacionamento muitos para muitos

---
+++ prisma-io/courses-modules/prisma/migrations/20220627225235_create_relation_many_to_many/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "courses_modules" (
+    "id" TEXT NOT NULL,
+    "fk_id_course" TEXT NOT NULL,
+    "fk_id_module" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "courses_modules_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "courses_modules" ADD CONSTRAINT "courses_modules_fk_id_course_fkey" FOREIGN KEY ("fk_id_course") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses_modules" ADD CONSTRAINT "courses_modules_fk_id_module_fkey" FOREIGN KEY ("fk_id_module") REFERENCES "modules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
